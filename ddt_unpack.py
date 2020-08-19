fe5adea13b49e7ddf2278e05dec3de1eb6e40b2b
York816 <york816@163.com>
2020-08-19 10:35:20 +0800

    "unpack method"

---
+++ ddt_unpack.py
@@ -0,0 +1,16 @@
+from ddt import ddt, data, unpack
+import unittest
+
+
+@ddt
+class Test(unittest.TestCase):
+    name = [['橙子', '橘子'], ['柠檬', '柚子']]
+
+    @data(*name)
+    @unpack
+    def test(self, name1, name2):
+        print(name1, name2)
+
+
+if __name__ == '__main__':
+    unittest.main()
